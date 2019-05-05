b526c34c062a8b5f2c37c178106a9b444cdc08b1
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-05-05 18:24:46 +0100

    Add q3a

---
+++ 2017/q3.c
@@ -0,0 +1,24 @@
+/** COMP0002 2017 Q3 */
+
+/**
+ * Q3(a)
+ * malloc:
+ *      Library function which requests the operating system to allocate a
+ *      consecutive block of memory for the program; returns pointer to start of
+ *      memory block upon success, returns NULL ptr upon failure.
+ * free:
+ *      Library function which informs the operating system to deallocate a
+ *      block of memory starting at the given pointer, i.e. to tell the OS that
+ *      the block of memory starting at the given pointer is no longer needed
+ *      and can be marked as available for other programs to use.
+ * cohesive function:
+ *      A function is considered cohesive within a module if it is tightly
+ *      related to other functions in the same module in terms of common
+ *      responsibility / functionality. Usually, the function is also coupled to
+ *      the shared data structures within the its residing module.
+ * variable scope:
+ *      The visibility of a symbol refering to a variable.
+ * syntax:
+ *      Rules of the C language which determins whether some sequence of
+ *      characters is in fact a valid C program.
+ */
