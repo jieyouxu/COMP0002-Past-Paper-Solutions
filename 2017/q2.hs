7fd1d060a4f7be908720837cd1d83e5ffb740e18
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-05-05 18:02:48 +0100

    Add q2a

---
+++ 2017/q2.hs
@@ -0,0 +1,29 @@
+{-
+    COMP0002 2017 Q2
+
+    Given `foldr`, `map`, `filter` without definition.
+-}
+{-
+    Q2(a)
+    1.  > 5 * 10 - 49
+        = 50 - 49
+        = 1
+    2.  > "speedy" = "speed"
+        Error: assignment (=) operator used for comparison
+    3.  > succ 9 * 10
+        = succ 90
+        = 91
+    4.  > div 37 21
+        = 1
+    5.  > 12 + "13"
+        Error: type mismatch
+            Expected Int for second argument of (+), but got [Char] instead
+-}
+-- Q2(b)
+fac :: Int -> Int
+fac 0 = 1
+fac n = n * fac (n - 1)
+
+sumFac :: Int -> Int
+sumFac 0 = 1
+sumFac n = fac n + sumFac (n - 1)
