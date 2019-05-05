91b433e8e12eaf2c2a0061600ee63a23e496e1be
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-05-05 19:02:54 +0100

    Add q4a

---
+++ q4.c
@@ -0,0 +1,17 @@
+/** COMP0002 2017 Q4 */
+
+/**
+ * Q4(a)
+ *
+ * variable lifetime:
+ *      The time period with respect to the program that the variable refers to
+ *      a valid memory address, given that the variable is declared and visible.
+ * file scope:
+ *      After some symbol is declared, the symbol is visible to all subsequent
+ *      parts of the program in the same translation unit.
+ * dereference:
+ *      Retrieving the value stored at the memory address referenced by a
+ *      pointer.
+ * heap memory:
+ *      Memory which can be dynamically allocated by the program.
+ */
