522a0a88fd6956a0654aaf99ce7cd363b340ea38
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-05-05 18:33:29 +0100

    Add q3b

--- 2017/q3.c
+++ 2017/q3.c
@@ -1,5 +1,8 @@
 /** COMP0002 2017 Q3 */
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * Q3(a)
  * malloc:
@@ -22,3 +25,40 @@
  *      Rules of the C language which determins whether some sequence of
  *      characters is in fact a valid C program.
  */
+
+// Q3(b)
+// Assuming <string.h> library functions cannot be used
+int lengthOfString(char* str)
+{
+    int len = 0;
+    char* current = str;
+    while (*current != '\0')
+    {
+        len++;
+        current++;
+    }
+
+    return len;
+}
+
+char* copyString(char* str)
+{
+    int len = lengthOfString(str);
+    char* copy = (char*) malloc(len * sizeof(char));
+
+    for (int i = 0; i < len; i++)
+        *(copy + i) = *(str + i);
+
+    return copy;
+}
+
+int main(void)
+{
+    printf("Q3(b): length of \"test\" is %d\n", lengthOfString("test"));
+    char* original = "123";
+    char* copy = copyString(original);
+    printf("Q3(b): copy of \"%s\" is \"%s\"\n", original, copy);
+    free(copy);
+
+    return 0;
+}
