32b79c3c4299019124b2d16da87b01c993ca0c56
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-05-05 17:26:44 +0100

    Add q1b

--- 2017/q1.c
+++ 2017/q1.c
@@ -12,6 +12,13 @@
 // =    5 - 6
 // =    -1 (int)
 
+// Q1b
+// A segmentation fault occurs if an attempt is made by a user-space C program
+// trying to access memory that is not owned by program. This causes the program
+// to terminate (crash) due to the Operating System trying to prevent illegal
+// memory access from corrupting memory of other user-space applications as well
+// as the kernel or OS.
+
 int main(void)
 {
     printf("Q1a: %d\n", 1 * 2 * 3 / 4 * 5 - 6);
