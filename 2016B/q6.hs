5a567421af177c99570095d697c35ec24e4e0a82
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-05-06 15:12:18 +0100

    Add q6a

---
+++ 2016B/q6.hs
@@ -0,0 +1,17 @@
+{-
+    COMP0002 2016B Q6
+-}
+
+{-
+    Q6(a)
+
+    recursive variant:
+        Some data that changes with each recursive call
+    lambda abstraction:
+        Anonymous function (without a name)
+    partial application:
+        A new function created from the original function with some of its 
+        parameters filled in via currying.
+    type class:
+        An interface which defines some operations available
+-}
