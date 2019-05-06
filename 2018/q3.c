8d60ccce6c538d64ea828da13f6054b4c5fc9a28
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-05-06 20:43:18 +0100

    Add q3b

--- 2018/q3.c
+++ 2018/q3.c
@@ -1,5 +1,8 @@
 /** COMP0002 2018 Q3 */
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * Q3(a)
  *  void:
@@ -13,3 +16,29 @@
  *  function declaration:
  *      Designating a symbol with a list of parameters to refer to a procedure.
  */
+
+/**
+ * Q3(b)
+ *  Linked List can be represented via node structs, with a key field and a
+ *  pointer to the next node:
+ */
+
+typedef struct node
+{
+    int value;
+    node* next;
+} node;
+
+void LinkedList_append(node* root, int value)
+{
+    if (root == NULL)
+        return;
+
+    node* newNode = malloc(sizeof(node));
+    newNode->value = value;
+
+    node* currentNode = root;
+    while (currentNode->next != NULL)
+        currentNode++;
+    currentNode->next = newNode;
+}
