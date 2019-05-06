b304f1f0cd621e54b1c085c2929450e9fda74d4c
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-05-06 11:58:51 +0100

    Add q3a

---
+++ 2016B/q3.c
@@ -0,0 +1,21 @@
+/** COMP0002 2016B Q3 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Q3(a)
+ *
+ * stack frame:
+ *      Represents function call:
+ *          - Return address (last invoking address)
+ *          - Arguments data
+ *          - Local variables
+ * local scope:
+ *      Visiblity within a function or a block.
+ * pointer arithmetic:
+ *      Ability to offset memory address relative to a base address via addition
+ *      and subtraction.
+ * function definition:
+ *      Implementing the actual body of a function.
+ */
