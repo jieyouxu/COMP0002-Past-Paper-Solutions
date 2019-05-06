5eb7b94f86bbe4a7f70282f6fdd97bf6f6d401b6
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-05-06 22:32:53 +0100

    Add q4b

--- 2018/q4.c
+++ 2018/q4.c
@@ -15,3 +15,26 @@
  *  stderr:
  *      Standard error stream output, usually the console unless redirected.
  */
+
+/**
+ * Q4(b)
+ *  A boolean value in C is represented by integers:
+ *      0 = false
+ *      1 = true
+ *  Memory minimization can be achieved by compacting booleans into a bit-field:
+ *  Perhaps a bit-field can be defined via an 1-byte (8-bits) `char` type, which
+ *  allows 8 separate boolean values to be stored:
+ *      char c1
+ *            b7 b6 b5 b4 b3 b2 b1 b0
+ *          [ 1  0  1  1  1  0  1  0  ]
+ *  Then each bit of c1, e.g. b2 can be either true or false. Extracting the
+ *  value involves bit-masks, e.g. to find the value of b4, use a bitmask:
+ *      b7 b6 b5 b4 b3 b2 b1 b0
+ *      0  0  0  1  0  0  0  0
+ *      1  0  1  1  0  0  0  0  &
+ *      =======================
+ *      0  0  0  1  0  0  0  0
+ *               ^
+ *               |
+ *               Value of b4 from the bit-field
+ */
