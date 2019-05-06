7470b81d695f7bf9402c59ed56064a9f0d50fce6
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-05-06 21:10:42 +0100

    Add q3c

--- 2018/q3.c
+++ 2018/q3.c
@@ -26,19 +26,71 @@
 typedef struct node
 {
     int value;
-    node* next;
+    struct node* next;
 } node;
 
-void LinkedList_append(node* root, int value)
+void LinkedList_append(struct node* root, int value)
 {
     if (root == NULL)
         return;
 
-    node* newNode = malloc(sizeof(node));
+    struct node* newNode = malloc(sizeof(node));
     newNode->value = value;
 
-    node* currentNode = root;
+    struct node* currentNode = root;
     while (currentNode->next != NULL)
         currentNode++;
     currentNode->next = newNode;
+}
+
+// Q3(c)
+
+#define MIN(a, b) (a < b ? a : b)
+
+int lengthOfString(char* s)
+{
+    if (s == NULL)
+        return 0;
+
+    int len = 0;
+    while (s[len] != '\0')
+        len++;
+    return len;
+}
+
+int find(char* s1, char* s2)
+{
+    if (s1 == NULL || s2 == NULL)
+        return -1;
+
+    int s1Length = lengthOfString(s1);
+    int s2Length = lengthOfString(s2);
+    int limit = s1Length - s2Length;
+
+    for (int i = 0; i < limit; i++)
+    {
+        int matchedSegmentLength = 0;
+
+        for (int j = 0; j < s2Length; j++)
+        {
+            if (s1[i + j] == s2[j])
+                matchedSegmentLength++;
+
+            if (matchedSegmentLength == s2Length)
+                return i;
+        }
+
+        matchedSegmentLength = 0;
+    }
+
+    return -1;
+}
+
+int main(void)
+{
+    char* s1 = "This is a sentence";
+    char* s2 = "sent";
+    printf("Q3(c): find(\"%s\", \"%s\") returns %d\n", s1, s2, find(s1, s2));
+
+    return 0;
 }
