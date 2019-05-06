abf60967693666937a747003f187c8287273cf38
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-05-06 11:28:23 +0100

    Add q2a

---
+++ 2016B/q2.hs
@@ -0,0 +1,20 @@
+{-
+    COMP0002 2016B Q2
+-}
+
+{-
+    Q2(a)
+
+    1.  > 1.0 / 3 + (-1)
+        = 0.33333 - 1
+        = -0.6666
+    2.  > "Abe" < "Abbey"
+        = False
+    3.  > 12 /= 12.00000000001
+        True
+    4.  > 7 * -4
+        Error: infix operator (*) and (-)
+    5.  > 'a' < "apple"
+        Error: type mismatch between Char and [Char]
+-}
+
