22cfe204839b2e77b1b972263f4ed18adc3406e5
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-05-06 12:24:56 +0100

    Add q4a

---
+++ 2016B/q4.c
@@ -0,0 +1,15 @@
+/** COMP0002 Q4 */
+
+/**
+ * Q4(a)
+ * virtual memory:
+ *      Memory managed by the operating system such that how a virtual memory
+ *      address is mapped to a physical memory address is managed by the OS.
+ * free:
+ *      Signal to the OS that a certain memory block starting at a given memory
+ *      address is no longer needed by the program, and can be deallocated.
+ * compound statement:
+ *      A statement list composed from multiple statements.
+ * variable declaration:
+ *      Designating a new symbol able to hold values of a specific type.
+ */
