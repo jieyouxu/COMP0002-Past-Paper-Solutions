605e16e2daabececd49c199d4e748dc9234c2336
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-05-06 14:11:43 +0100

    Add q5a

---
+++ 2016B/q5.hs
@@ -0,0 +1,23 @@
+{-
+    COMP0002 Q5
+
+    Given `map`, `foldr`, `filter`
+-}
+
+{-
+    Q5(a)
+
+    pre-condition:
+        Constraints on parameters and global states before function is executed.
+    
+    side-effect:
+        Modifying some external state outside of the function.
+    
+    operator-section:
+        Partial application of an operator, e.g.
+            Left section: (3 *)
+            Right section: (* 3)
+    
+    higher-order function:
+        A function which takes another function as an parameter
+-}
