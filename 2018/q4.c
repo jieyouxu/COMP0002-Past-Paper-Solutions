f97c7289d99ce475c1aafe8591405576d5fb1a7b
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-05-06 22:24:27 +0100

    Add q4a

---
+++ 2018/q4.c
@@ -0,0 +1,17 @@
+/** COMP0002 2018 Q4 */
+
+/**
+ * Q4(a)
+ *
+ *  input buffering:
+ *      Store (accumulate) input into some array for later processing. If
+ *      buffer reaches or is near its size limit, process and clear input stored
+ *      in buffer.
+ *  function definition:
+ *      Supplying the implementation for a function's declaration.
+ *  file scope:
+ *      When a symbol's visibility is the same compilation unit after
+ *      it has been declared.
+ *  stderr:
+ *      Standard error stream output, usually the console unless redirected.
+ */
