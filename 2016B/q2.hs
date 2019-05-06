98c8c0dbcfae28741a57b1c5ae217a06e24065b9
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-05-06 11:28:35 +0100

    Add q2b

--- 2016B/q2.hs
+++ 2016B/q2.hs
@@ -18,3 +18,36 @@
         Error: type mismatch between Char and [Char]
 -}
 
+{-
+    Q2(b)
+    Given
+
+    1. maximum' :: Ord a => [a] -> [a]
+    2. maximum' [] = error 'maximum of empty list'
+    3. maximum' [x] = [x]
+    4. maximum' (y:xs)
+    5.     | x < maxTail = x
+    6.     | else = maxTail
+    7.     | where maxTail = maximum (x:xs)
+
+    1. Should be
+        maximum' :: Ord a => [a] -> a
+        Since it is supposed to find one maximum element
+    2. Should be
+        maximum' [] = error "maximum of empty list"
+        As the error message is [Char] not Char
+    3. Should be
+        maximum' [x] = x
+        Since maximum' finds an element
+    4. Should be
+        maximum' (x:xs)
+        Since definition refers to `x`, the head of the input list
+    6. Should be
+        | otherwise = maxTail
+        Since pattern matching uses `otherwise` for catch
+    7. Should be
+        where maxTail = maximum' xs
+        Firstly, where is not part of the pattern guard definition
+        Secondly, should be maximum' not maximum
+        Thirdly, shpuld be maximum' xs, which is the tail of the input list
+-}
