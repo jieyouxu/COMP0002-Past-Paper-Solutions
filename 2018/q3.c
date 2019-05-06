6cd3222d6d79581f1ba2e399c4b43ca08cadfe87
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-05-06 20:37:44 +0100

    Add q3a

---
+++ 2018/q3.c
@@ -0,0 +1,15 @@
+/** COMP0002 2018 Q3 */
+
+/**
+ * Q3(a)
+ *  void:
+ *      Keyword indicating no parameters, no return type and arbitrary type in
+ *      the case of a (void *).
+ *  linking:
+ *      Linker combines object files produced from compilation and produce an
+ *      executable file after resolving their dependencies.
+ *  literal value:
+ *      Expressions which represent exactly their corresponding value.
+ *  function declaration:
+ *      Designating a symbol with a list of parameters to refer to a procedure.
+ */
