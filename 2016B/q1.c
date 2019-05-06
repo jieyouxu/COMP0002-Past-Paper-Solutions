5d6402fc4bb44a7d321518951325411247901626
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-05-06 09:41:08 +0100

    Add q1a

---
+++ 2016B/q1.c
@@ -0,0 +1,21 @@
+/** COMP0012 2016B Q1 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Q1(a)
+ * >        9 / 3 / 2 * 6 + 2 * 1.5
+ *      =   3 / 2 * 6 + 2 * 1.5
+ *      =   1 * 6 + 3.0
+ *      =   6 + 3.0
+ *      =   9.0
+ *      double
+ */
+
+int main(void)
+{
+    printf("Q1(a): %f\n", 9 / 3 / 2 * 6 + 2 * 1.5);
+
+    return 0;
+}
