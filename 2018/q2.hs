7954ea4b85286f817a487a760d92647d3e21c497
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-05-06 20:05:11 +0100

    Add q2a

---
+++ 2018/q2.hs
@@ -0,0 +1,21 @@
+{-
+    COMP0002 2018 Q2
+    
+    Given `foldr`, `map`, `filter`.
+-}
+
+{-
+    Q2(a)
+        1.  > 4 + 6.0
+            = 10.0
+        2.  > 27 == "27"
+            Error: type mismatch between Int and [Char]
+        3.  > (\v d -> v d) (\d -> d) 2
+            = 2
+        4.  > not ( True && (not True) )
+            = not ( True && False )
+            = not False
+            = True
+        5.  > - read "3"
+            = -3
+-}
